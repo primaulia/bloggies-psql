81746ace27f182992604c50ef2df600a7782a25e
Prima Aulia Gusta <primaulia@gmail.com>
2017-08-08 15:28:38 +0800

    latest before association

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,28 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+
   def index
-    render html: 'show all posts'
+    render html: "show all posts by #{current_user.id}"
+  end
+
+  def show
+    render html: 'show one post'
+  end
+
+  def new
+    render html: 'show form to create new post'
+  end
+
+  def edit
+    render html: 'show form to edit existing post'
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def foo
   end
 end
