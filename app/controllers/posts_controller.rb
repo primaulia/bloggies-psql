65d0555d80e159230ff36d1c876756cbc8e7ab19
Prima Aulia Gusta <primaulia@gmail.com>
2017-08-11 11:27:46 +0800

    image helper and open weather api

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,15 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:show]
 
   def index
+    weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=singapore&appid=49c8b93b67e42d078d572c8368f538a8'
+    weather_response = HTTParty.get(weather_url)
+
     news_url = 'https://newsapi.org/v1/articles?source=buzzfeed&sortBy=latest&apiKey=6a1b87ded86d47cf8f7b18a230f094bf'
     response = HTTParty.get(news_url)
 
     @news_data = response
-    # render json: @news_data
+    @weather_data = weather_response
+    # render json: @weather_data
 
     @all_posts = current_user.posts
     @new_post = current_user.posts.new
