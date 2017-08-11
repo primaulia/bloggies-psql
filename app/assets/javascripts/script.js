d05133d3039610f703b9a7d42bd4c9d0c252abd9
Prima Aulia Gusta <primaulia@gmail.com>
2017-08-11 16:18:34 +0800

    setup

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -4,13 +4,13 @@
 // })
 
 $(document).on('turbolinks:load', function () {
-  $('.static').ready(function () {
+  $('.home').ready(function () {
     // check if we're at static pages
     // load on static pages views
     alert('run ajax for static views only')
   })
 
-  $('.posts').ready(function () {
+  $('.posts.show').ready(function () {
     // check if we're at posts pages
     // load on posts views
     alert('run ajax for posts views only')
